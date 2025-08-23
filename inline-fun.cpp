7268de52c9bf8cc6a85fed5a847f372eb82d7dee
varunrnc <barunpandit143@gmail.com>
2025-08-23 22:24:40 +0530

    inline function in cpp

--- inline-fun.cpp
+++ inline-fun.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Tpoint{
+public:
+    inline int square(int a);
+};
+
+inline int Tpoint::square(int a)
+{
+    return a*a;
+}
+
 int main()
 {
-    cout << "Varun" << endl;
+    Tpoint obj;
+    cout << "The Square of a Number is : " << obj.square(10);
     return 0;
 }
